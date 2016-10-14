a0dc59d328626e67a1bc49bf7f305c9a2a740b1d
Guillermo Gutierrez <guillermo20@gmail.com>
2016-10-13 21:12:05 -0500

    removing legacy code

--- app/src/main/java/com/example/guillermo/popularmovies/backgroundtasks/FetchPopularMoviesTask.java
+++ app/src/main/java/com/example/guillermo/popularmovies/backgroundtasks/FetchPopularMoviesTask.java
@@ -173,17 +173,4 @@ public class FetchPopularMoviesTask extends AsyncTask<String, Void, List<MovieIt
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
-    public Boolean isOnline() {
-        try {
-            Process p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
-            int returnVal = p1.waitFor();
-            boolean reachable = (returnVal==0);
-            return reachable;
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return false;
-    }
-
 }
